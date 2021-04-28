eff62137b15519079df979538eff5ae803025341
shubhamlondhe1996 <shubham.londhe@vamstar.io>
2021-04-28 12:54:38 +0530

    test deploy

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #      os.path.join(BASE_DIR, 'frontend/build/static')
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
