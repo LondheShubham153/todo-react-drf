d7b4675fdf8c9bb747cf06a32c5f26062e581c9f
shubhamlondhe1996 <shubham.londhe@vamstar.io>
2021-04-28 14:07:27 +0530

    added static

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'frontend/build/static')
-]
+# STATICFILES_DIRS = [
+#      os.path.join(BASE_DIR, 'frontend/build/static')
+# ]
+
+STATIC_ROOT = os.join(BASE_DIR,'staticfiles')
