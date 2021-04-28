76f089ac07065b88df3d55561e653204433623cd
shubhamlondhe1996 <shubham.londhe@vamstar.io>
2021-04-28 13:09:19 +0530

    test deploy

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
