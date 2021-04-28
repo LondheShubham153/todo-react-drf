1bac252f6515355a1c7b412ab786f22c4de17803
shubhamlondhe1996 <shubham.londhe@vamstar.io>
2021-04-28 14:04:36 +0530

    test deploy

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -20,5 +20,4 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
 ]
